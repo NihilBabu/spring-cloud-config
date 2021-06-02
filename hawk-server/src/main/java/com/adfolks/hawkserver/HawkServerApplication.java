69a44a9aec9dae48f0331957774d91304b395042
zycon <tibin@zycon.in>
2021-06-02 10:56:29 +0400

    updates

--- hawk-server/src/main/java/com/adfolks/hawkserver/HawkServerApplication.java
+++ hawk-server/src/main/java/com/adfolks/hawkserver/HawkServerApplication.java
@@ -2,8 +2,10 @@ package com.adfolks.hawkserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 public class HawkServerApplication {
 
 	public static void main(String[] args) {
