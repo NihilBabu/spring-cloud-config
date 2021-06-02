dc891c5e63572a09c0a9bfd9203e3d52bcebd31c
zycon <tibin@zycon.in>
2021-06-02 15:59:44 +0400

    updates? ??

--- logic/src/main/java/com/adfolks/logics/Controller.java
+++ logic/src/main/java/com/adfolks/logics/Controller.java
@@ -1,13 +1,19 @@
 package com.adfolks.logics;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/logic")
+@RefreshScope
 public class Controller {
+    @Value("${client.name}")
+    String clientName;
+
     @RequestMapping("/main")
     public String getLoginHome(){
-        return "Main Business application logic";
+        return "Client Name: ".concat(clientName) ;
     }
 }
