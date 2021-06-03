d3ca72754c15b12382ea7bb104a38b22734552e5
Tibin Lukose <tibin@zycon.in>
2021-06-03 09:24:45 +0400

    config changes

--- runlogic/src/main/java/com/adfolks/runlogic/API.java
+++ runlogic/src/main/java/com/adfolks/runlogic/API.java
@@ -1,10 +1,12 @@
 package com.adfolks.runlogic;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RefreshScope
 public class API {
     @Value("${client.name}")
     String name;
