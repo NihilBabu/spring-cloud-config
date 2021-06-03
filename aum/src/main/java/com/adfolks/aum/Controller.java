7e22df5a9d33ce91b1305e732b994002734ed0d1
NihilBabu <babunihil@gmail.com>
2021-06-03 10:28:25 +0530

    remote config added for aum service

--- aum/src/main/java/com/adfolks/aum/Controller.java
+++ aum/src/main/java/com/adfolks/aum/Controller.java
@@ -1,13 +1,17 @@
 package com.adfolks.aum;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/aum")
 public class Controller {
+    @Value("${client.name}")
+    String clientName;
+
     @RequestMapping("/home-page")
     public String getLoginHome(){
-        return "Welcome to login home page";
+        return "Welcome to login home page "+ clientName ;
     }
 }
