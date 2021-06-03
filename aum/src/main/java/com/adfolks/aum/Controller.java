7ca8f6cecf11ae30ba7273ad940e6335f8012af2
Tibin Lukose <tibin@zycon.in>
2021-06-03 09:23:33 +0400

    config changes

--- aum/src/main/java/com/adfolks/aum/Controller.java
+++ aum/src/main/java/com/adfolks/aum/Controller.java
@@ -1,11 +1,13 @@
 package com.adfolks.aum;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/aum")
+@RefreshScope
 public class Controller {
     @Value("${client.name}")
     String clientName;
