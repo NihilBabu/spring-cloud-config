37605b1c14b7eb9a594ebca8747ae8be1fc0fd3b
Tibin Lukose <tibin@zycon.in>
2021-06-14 15:29:35 +0400

    config changes

--- alerts/src/main/java/com/adfolks/alerts/API.java
+++ alerts/src/main/java/com/adfolks/alerts/API.java
@@ -4,18 +4,23 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 @RefreshScope
 public class API {
     @Value("${client.name}")
     String name;
+    @Value("${timeservice.url}")
+    String timeUrl;
+
     @RequestMapping("/test")
     public String test(){
         return "Hello "+name;
     }
     @RequestMapping("/time")
     public String timeAPI(){
-        return "Hello World!!!";
+        RestTemplate restTemplate=new RestTemplate();
+        return restTemplate.getForObject(timeUrl,String.class);
     }
 }
